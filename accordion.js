400ccdcf1f9c2c8c251a3dcde3a81e49747c38f1
Dhruvik6 <dhruvikk6@gmail.com>
2023-11-21 23:03:20 -0400

    Update accordion.js

--- accordion.js
+++ accordion.js
@@ -26,6 +26,42 @@ $(document).ready(function(){
         if($(".accordion-msg").attr('aria-expanded') == 'true'){
             $(".accordion-msg").addClass("scale_active");
         }
+                    "use strict";
+$(document).ready(function() {
+    // $('.theme-loader').addClass('loaded');
+    $('.theme-loader').animate({
+        'opacity': '0',
+    }, 1200);
+    setTimeout(function() {
+        $('.theme-loader').remove();
+    }, 2000);
+    // $('.pcoded').addClass('loaded');
+});
+
+if ( 'classList' in document.documentElement ) {
+  hasClass = function( elem, c ) {
+    return elem.classList.contains( c );
+  };
+  addClass = function( elem, c ) {
+    elem.classList.add( c );
+  };
+  removeClass = function( elem, c ) {
+    elem.classList.remove( c );
+  };
+}
+else {
+  hasClass = function( elem, c ) {
+    return classReg( c ).test( elem.className );
+  };
+  addClass = function( elem, c ) {
+    if ( !hasClass( elem, c ) ) {
+      elem.className = elem.className + ' ' + c;
+    }
+  };
+  removeClass = function( elem, c ) {
+    elem.className = elem.className.replace( classReg( c ), ' ' );
+  };
+}
         else{
             $(".accordion-msg").removeClass("scale_active");
         }
